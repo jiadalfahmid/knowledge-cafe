cacb0406b49168a5a6b13a1caa62177a4c9eb00e
jiadalfahmid <jiadalfahmid@gmail.com>
2024-10-21 20:55:17 +0600

    Starting Knowledge Cafe with React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './components/Blogs/blogs'
+import Header from './components/header/header'
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
